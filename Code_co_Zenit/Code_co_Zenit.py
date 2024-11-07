20052d26b7c838c4f67e4d26332e901317e17716
huamaniERICK <erickhuamaniccahuay10@gmail.com>
2024-11-07 10:23:58 -0500

    actualizo el landing page

--- Code_co_Zenit/Code_co_Zenit.py
+++ Code_co_Zenit/Code_co_Zenit.py
@@ -1,39 +1,24 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
+from .componentes.navbar import navbar
+from .componentes.seccion import seccion
+from .componentes.cambiar_tema import cambiar_tema
 
-from rxconfig import config
+# si deseamos tener border en nuestros componentes en relacion a nuestra ventana rx.container
+# si deseamos que nuestros componetes ocupen el 100% en relacion a nuestra ventana rx.box
+# si deseamos que nuestros componentes se han flexibles en relacion a nuestra ventana dependiendo su distribucion rx.vstack o rx.hstack (rx.flex)
+def index()->rx.Component:
+  return rx.box(
+    navbar(),
+    cambiar_tema(),
+    seccion(),
+  )
 
-
-class State(rx.State):
-    """The app state."""
-
-    ...
-
-
-def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
-
-
-app = rx.App()
+app=rx.App(
+  theme=rx.theme(
+    appearance="light",
+    has_background=True,
+    radius="large",
+    accent_color="teal",
+  )
+)
 app.add_page(index)
